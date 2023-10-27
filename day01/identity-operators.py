cf6fbeffd321e2b2102b90dee0114834f9831cb1
Pom <62348850+Pom2012@users.noreply.github.com>
2023-10-27 11:44:31 -0400

    added python operators

---
+++ day01/identity-operators.py
@@ -0,0 +1,10 @@
+"""
+    is  -   Returns true if both operands are the same object
+    is not  -   Returns true if both operands are not the same object
+"""
+
+s = 'Java'
+s2 = 'Java'
+
+print(s is s2)  #true
+print(s is not s2)  #false
---
+++ day01/membership_operators.py
@@ -0,0 +1,12 @@
+#   membership operators
+
+"""
+        in - Returns true if the specified value is presented in the object
+        not in - Returns true if the specified value is not presented in the object
+"""
+#   in Java we use contains() method
+
+s = 'Java C# C++ '
+
+print('Python' not in s) #  True
+print('Python' in s) #  False
