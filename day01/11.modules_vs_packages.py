23a253fd5f789e2aef3aa98589d77f903c19c348
Pom <62348850+Pom2012@users.noreply.github.com>
2023-10-27 12:53:16 -0400

    added user input and other py files

---
+++ day01/11.modules_vs_packages.py
@@ -0,0 +1,19 @@
+"""
+    Difference Between Module and Package in Python
+    In Python, both modules and packages organize and structure the code but serve different purposes.
+
+    In simple terms, a module is a single file containing python code,
+    whereas a package is a collection of modules that are organized in a directory hierarchy.
+
+    Module: It can be a simple Python file (.py extensions) that contains collections of functions and global variables.
+        Required Files: Only Python File(.py format)
+        How to import: import module_name
+        Example: math, random, os, datetime, csv
+        Purpose: 	Code organization
+    Package: A Package is a collection of different modules with an _init_.py file.
+        Required Files: ‘_init_.py’ file and python files
+        How to import: import package_name.module_name
+         Example: Numpy, Pandas, Matplotlib, django
+         Purpose: Code distribution and reuse
+
+"""
---
+++ day01/9.user_input.py
@@ -0,0 +1,8 @@
+"""
+        Scanner funcation in Java
+        We can ask the user for input by using the input() method
+"""
+
+name = input("Enter you name")
+
+print(name)
