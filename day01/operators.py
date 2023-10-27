2ff8da814f6ce931b5c493951379b6a455741a97
Pom <62348850+Pom2012@users.noreply.github.com>
2023-10-27 11:16:25 -0400

    added new python classes

---
+++ day01/operators.py
@@ -0,0 +1,23 @@
+# arithmetic: +, -, *, /, %
+
+print(10-2)         #   8
+print(10+2)         #   12
+print(10*2)         #   20
+print(10/2)         #   5.0
+print(int(10/2))    #   5
+print(10%2)         #   0
+print(10%3)         #   1
+
+a =200;
+b =200;
+c =200;
+print(a)    #200
+
+a+=100;
+print(a)    #300
+
+b-=100;
+print(b)    #100
+
+c/=100;
+print(c)    #2.0
